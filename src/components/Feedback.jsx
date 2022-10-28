dfe63c78616c8e97ac2c18b10fbfd3e0fe3c949a
osvyatobog <osvyatobog@gmail.com>
2022-10-28 04:10:12 +0300

    step1

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,80 +1,65 @@
 import React, { Component } from 'react';
-import { StatisticData } from './Feedback.styled';
+import {
+  StatisticData,
+  Buttons,
+  Button,
+  Result,
+  DataRecord,
+} from './Feedback.styled';
 
 export class Feedback extends Component {
-  // static defaultProps = {
-  //   step: 1,
-  // };
+  static defaultProps = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-  constructor() {
-    super();
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-  }
-  // for (let i = 0; i < 5; i++) {
-  //   console.log(this.state.good);
-  //   this.setState(prevState => {
-  //     console.log(prevState.good);
-  //   });
-  // }
+  state = {
+    good: this.props.good,
+    neutral: this.props.neutral,
+    bad: this.props.bad,
+  };
 
-  handleGood = event => {
-    console.log(event.type);
-    const { target } = event;
-    setTimeout(() => {
-      console.log(target);
-    }, 500);
+  handleGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  handleNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  handleBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
   };
 
   render() {
-    // const step = this.props;
-
     return (
       <StatisticData>
-        <span>Good: {this.state.good}</span>
-        <span>Neutral: {this.state.neutral}</span>
-        <span>Bad: {this.state.bad}</span>
-        <button type="button" onClick={this.handleGood}>
-          Good
-        </button>
-        <button type="button" onClick={this.handleNeutral}>
-          Neutral
-        </button>
-        <button type="button" onClick={this.handleBad}>
-          Bad
-        </button>
+        <h1>Please leave feedback</h1>
+        <Buttons>
+          <Button type="button" onClick={this.handleGood}>
+            Good
+          </Button>
+          <Button type="button" onClick={this.handleNeutral}>
+            Neutral
+          </Button>
+          <Button type="button" onClick={this.handleBad}>
+            Bad
+          </Button>
+        </Buttons>
+        <h2>Statistics</h2>
+        <Result>
+          <DataRecord>Good: {this.state.good}</DataRecord>
+          <DataRecord>Neutral: {this.state.neutral}</DataRecord>
+          <DataRecord>Bad: {this.state.bad}</DataRecord>
+        </Result>
       </StatisticData>
     );
   }
 }
-
-// export class Feedback extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       good: 0,
-//       neutral: 0,
-//       bad: 0,
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Please leave feedback</h1>
-//         {/* <button type="button">Good</button>
-//         <button type="button">Neutral</button>
-//         <button type="button">Bad</button> */}
-//         <h2>Statistics</h2>
-//         <StatisticData>
-//           <span>Good: {this.state.good}</span>
-//           <span>Neutral: {this.state.neutral}</span>
-//           <span>Bad: {this.state.bad}</span>
-//         </StatisticData>
-//       </div>
-//     );
-//   }
-// }
--- src/components/Feedback.styled.js
+++ src/components/Feedback.styled.js
@@ -2,9 +2,34 @@ import styled from "styled-components"
 
 export const StatisticData = styled.div`
 display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-around;
   align-items: center;
-  background: peachpuff;
+  background: teal;
   padding: 20px 0;
   border: 1px solid white;`
+
+export const Buttons = styled.div`
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;`
+
+export const Button = styled.button`
+font-weight: 700;
+font-size: 20px;
+width: 200px;
+padding: 8px;
+box-shadow: 0px 2px 2px 0px gray;
+border: 1px solid grey;
+border-radius: 4px;
+margin-right: 15px;`
+
+export const Result = styled.div``
+
+export const DataRecord = styled.span`
+display: block;
+padding: 10px;
+font-size: 20px;
+`
